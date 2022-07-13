34578e12d62a11dd6eaa3da02d667939c322473d
AndreaPallotta <andreapallotta9@gmail.com>
2022-07-12 23:42:17 -0400

    Added CRUD functions

--- backend/models/dao/Users.dao.js
+++ backend/models/dao/Users.dao.js
@@ -20,6 +20,23 @@ const tableFields = [
     name: 'password',
     type: 'TEXT',
   },
+  {
+    name: 'accessToken',
+    type: 'TEXT',
+  },
+  {
+    name: 'refreshToken',
+    type: 'TEXT',
+  },
+];
+
+const insertFields = [
+  'first_name',
+  'last_name',
+  'email',
+  'password',
+  'accessToken',
+  'refreshToken',
 ];
 
 class UsersDAO {
@@ -27,8 +44,32 @@ class UsersDAO {
     this.dao = dao;
   }
 
-  createTable() {
+  create() {
     return this.dao.createTable('users', tableFields);
+  }
+
+  get(fields, whereFields, params) {
+    return this.dao.getFromTable('users', fields, whereFields, params);
+  }
+
+  getAll(whereFields, params) {
+    return this.dao.getAllFromTable('users', whereFields, params);
+  }
+
+  insert(...values) {
+    return this.dao.insertIntoTable('users', insertFields, [...values]);
+  }
+
+  update(setFields, whereFields, values, extra) {
+    return this.dao.updateTable('users', setFields, whereFields, values, extra);
+  }
+
+  delete(whereFields, values) {
+    return this.dao.deleteFromTable('users', whereFields, values);
+  }
+
+  custom(query, params) {
+    return this.dao.run(query, params);
   }
 }
 
--- backend/models/db.js
+++ backend/models/db.js
@@ -13,20 +13,9 @@ class DAO {
     });
   }
 
-  createTable(tableName, fields) {
-    const tableFields = fields.map(
-      ({ name, type, attr }, index) =>
-        `${name} ${type} ${attr || ''}${index < fields.length - 1 ? ',\n' : ''}`
-    );
-    const query = `CREATE TABLE IF NOT EXISTS ${tableName} (
-      ${tableFields.join(' ')}
-    )`;
-    return query;
-  }
-
   run(query, params = []) {
     return new Promise((resolve, reject) => {
-      this.db.run(sql, params, (err) => {
+      this.db.run(query, params, (err) => {
         if (err) {
           Logger.error(`Error running query ${sql}: ${err}`);
           reject(err);
@@ -34,6 +23,78 @@ class DAO {
         resolve({ id: this.lastID });
       });
     });
+  }
+
+  createTable(tableName, fields) {
+    const tableFields = fields.map(
+      ({ name, type, attr }) => `${name} ${type} ${attr || ''}`
+    );
+    const query = `CREATE TABLE IF NOT EXISTS ${tableName} (
+      ${tableFields.join(', ')}
+    )`;
+    return this.run(query.trim());
+  }
+
+  getFromTable(tableName, fields, whereFields, params) {
+    if (fieldNames.length !== params.length) return;
+    const whereFieldsString = whereFields.map((field) => `${field}=?`);
+    const query = `SELECT ${fields.join()}
+    FROM ${tableName}
+    ${
+      whereFields.length > 0 ? `WHERE ${whereFieldsString.join(' AND ')}` : ''
+    }`;
+
+    return this.run(query, params);
+  }
+
+  getAllFromTable(tableName, whereFields, params) {
+    const whereFieldsString = whereFields.map((field) => `${field}=?`);
+    const query = `SELECT *
+    FROM ${tableName}
+    ${
+      whereFields.length > 0 ? `WHERE ${whereFieldsString.join(' AND ')}` : ''
+    }`;
+
+    return this.run(query, params);
+  }
+
+  insertIntoTable(tableName, fieldNames, params) {
+    if (fieldNames.length !== params.length) return;
+    const placeholders = Array(fieldNames.length).fill('?');
+
+    const query = `INSERT INTO ${tableName}
+      (${fieldNames.join(',')})
+      VALUES (${placeholders.join(',')})`;
+
+    return this.run(query.trim(), params);
+  }
+
+  updateTable(tableName, setFields, whereFields, params, extra) {
+    if (setFields.length + whereFields.length !== params.length) return;
+    const setFieldsString = setFields.map((field) => `${field}=?`);
+    const whereFieldsString = whereFields.map((field) => `${field}=?`);
+
+    const query = `UPDATE ${tableName}
+      SET ${setFieldsString.join()}
+      ${
+        whereFields.length > 0 ? `WHERE ${whereFieldsString.join(' AND ')}` : ''
+      }
+      ${extra || ''}
+  `;
+
+    return this.run(query.trim(), params);
+  }
+
+  deleteFromTable(tableName, whereFields, params) {
+    if (whereFields.length !== params.length) return;
+    const whereFieldsString = whereFields.map((field) => `${field}=?`);
+
+    const query = `DELETE FROM ${tableName}
+    ${
+      whereFields.length > 0 ? `WHERE ${whereFieldsString.join(' AND ')}` : ''
+    }`;
+
+    return this.run(query.trim(), params);
   }
 }
 

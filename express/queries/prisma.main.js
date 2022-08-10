abaa981d476df885e3a8e217dfc44e13771f9471
AndreaPallotta <andreapallotta9@gmail.com>
2022-08-10 12:35:57 -0400

    Added prisma

--- express/queries/prisma.main.js
+++ express/queries/prisma.main.js
@@ -1,21 +1,21 @@
-import { PrismaClient } from '@prisma/client';
-import Logger from '../logging/logger';
+const { PrismaClient } = require('@prisma/client');
+const Logger = require('@log/logger');
 
 const prisma = new PrismaClient();
 
 const main = async () => {
-  await prisma.$connect();
+    await prisma.$connect();
 };
 
 main()
-  .then(async () => {
-    await prisma.$disconnect();
-    Logger.debug('Successfully disconnected');
-  })
-  .catch(async () => {
-    Logger.error(`Failed to connect database: ${e}`);
-    await prisma.$disconnect();
-    process.exit(1);
-  });
+    .then(async () => {
+        await prisma.$disconnect();
+        Logger.debug('Successfully disconnected');
+    })
+    .catch(async (e) => {
+        Logger.error(`Failed to connect database: ${e}`);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
 
 module.exports = prisma;
